a06088f2218c1a73c6e10550af70fbe85d681e95
Alejandro De Groote <codebyalejandro@gmail.com>
2024-03-24 05:03:55 +0100

    Updated Windows executable build script

--- build_tools/build_executable.bat
+++ build_tools/build_executable.bat
@@ -1,5 +1,14 @@
+@echo off
+
 rem Get the directory path of the batch file
-set "batch_dir=%~dp"
-echo Directory of the batch file: %batch_dir%
-exit /b
-pyinstaller "%batch_dir%\main.py" --name "PDF facturatie programma" --onedir --add-data "%batch_dir%\..\images\":images/ --add-data "%batch_dir%\..\defaults\":defaults/ --exclude-module PIL --clean -y -w
+set "batch_dir=%~dp0"
+
+@echo on
+
+pyinstaller "%batch_dir%\..\app\main.py" ^
+	--name "PDF facturatie programma" ^
+	--onedir ^
+	--add-data="%batch_dir%\..\images\;images" ^
+	--add-data="%batch_dir%\..\defaults\;defaults" ^
+	--exclude-module PIL ^
+	--clean -y -w
